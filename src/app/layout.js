0ddcd8ee5ec4ba603960e9ec502547a128c69e0b
chandrakanth93986 <chandu93986@gmail.com>
2024-05-20 15:44:43 +0530

    Create route for register and some other features.

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Navbar />
         <div className="m min-h-screen">
+          <Toaster position="top-center"/>
           {children}
         </div>
         <Footer />
